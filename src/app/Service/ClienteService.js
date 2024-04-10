2a530670815b9a9f7263f10d58afeb5dc72fe3f6
Darwin Javier Silva Gadea <115656897+Infil505@users.noreply.github.com>
2024-04-09 21:43:39 -0600

    probar

---
+++ src/app/Service/ClienteService.js
@@ -0,0 +1,49 @@
+import axiosApi from './ApiService';
+
+// Función para crear un nuevo cliente
+async function crearCliente(nuevoCliente) {
+  try {
+    const response = await axiosApi.post('/cliente', nuevoCliente, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log('Nuevo cliente creado:', response.data);
+  } catch (error) {
+    console.error('Error al crear el cliente:', error);
+  }
+}
+
+// Función para obtener todos los clientes
+async function obtenerClientes() {
+  try {
+    const response = await axiosApi.get('/cliente');
+    console.log('Clientes:', response.data);
+  } catch (error) {
+    console.error('Error al obtener los clientes:', error);
+  }
+}
+
+// Función para actualizar un cliente
+async function actualizarCliente(cedula, clienteActualizado) {
+  try {
+    const response = await axiosApi.put(`/cliente/${cedula}`, clienteActualizado, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log('Cliente actualizado:', response.data);
+  } catch (error) {
+    console.error('Error al actualizar el cliente:', error);
+  }
+}
+
+// Función para eliminar un cliente
+async function eliminarCliente(cedula) {
+  try {
+    const response = await axiosApi.delete(`/cliente/${cedula}`);
+    console.log('Mensaje del servidor:', response.data);
+  } catch (error) {
+    console.error('Error al eliminar el cliente:', error);
+  }
+}
