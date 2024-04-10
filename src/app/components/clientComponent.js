52d52981fdd450b0ab3185a2a34f099d326deb47
Quintanilla1299 <suke12k17@gmail.com>
2024-04-10 02:26:00 -0600

    caso 21

--- src/app/components/clientComponent.js
+++ src/app/components/clientComponent.js
@@ -1,33 +1,24 @@
-//import { obtenerClientes } from "../Service/ClienteService.js";
+import { obtenerClientes, crearCliente } from "../Service/ClienteService.js";
 
-function send() {
+async function send() {
   const clienteData = {
-      cedula: $("#cedula").val(),
-      nombre: $("#nombre").val(),
-      apellidos: $("#apellidos").val(),
-      estadoCivil: $("#estadoCivil").val(),
-      direccion: $("#direccion").val(),
-      profesion: $("#profesion").val(),
-      nacionalidad: $("#nacionalidad").val()
+    cedula: $("#cedula").val(),
+    nombre: $("#nombre").val(),
+    apellidos: $("#apellidos").val(),
+    estadoCivil: $("#estadoCivil").val(),
+    direccion: $("#direccion").val(),
+    profesion: $("#profesion").val(),
+    nacionalidad: $("#nacionalidad").val()
   };
-  console.log(clienteData)
-  fetch('http://localhost:8080/cliente', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(clienteData)
-  })
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Error al enviar los datos del cliente');
-      }
-      console.log('Cliente creado exitosamente');
-      // Aquí puedes realizar cualquier acción adicional después de que se envíen los datos, como cargar una nueva tabla, etc.
-  })
-  .catch(error => {
-      console.error('Error al enviar los datos del cliente:', error);
-  });
+  console.log(clienteData);
+
+  try {
+    await crearCliente(clienteData);
+    // Si no hay errores en la creación del cliente, puedes cargar la tabla
+    cargarTabla();
+  } catch (error) {
+    console.error('Error al enviar los datos del cliente:', error);
+  }
 }
 
 function update() {
@@ -38,7 +29,7 @@ function destroy() {
 
 $(document).ready(function () {
   console.log("esta levantando");
-    cargarTabla();
+  cargarTabla();
 });
 
 async function cargarTabla() {
@@ -46,20 +37,19 @@ async function cargarTabla() {
     console.log("esta levantando");
     const response = await obtenerClientes();
     console.log('Clientes obtenidos:', response);
-    $("#data-tableClient").empty(); 
-    data.forEach(cliente => {
-     
-        let filaHTML = `<tr data-ced="${cliente.cedula}">
-            <td>${cliente.cedula}</td>
+    $("#data-tableClient").empty();
+    response.data.forEach(cliente => {
+      let filaHTML = `<tr data-ced="${cliente.cedula}">
             <td>${cliente.nombre}</td>
             <td>${cliente.apellidos}</td>
             <td>${cliente.estadoCivil}</td>
+            <td>${cliente.cedula}</td>
             <td>${cliente.direccion}</td>
             <td>${cliente.profesion}</td>
             <td>${cliente.nacionalidad}</td>
             <td><input type="checkbox" class="checkbox-accion" onchange=""></td>
         </tr>`;
-        $("#data-tableClient").append(filaHTML);
+      $("#data-tableClient").append(filaHTML);
     });
   } catch (error) {
     console.error('Error al obtener los clientes:', error);
